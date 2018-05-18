848217efe412a2d4664f86e913ad02cc49673602
miton18 <remi+github@collignon-ducret.fr>
2018-05-18 15:13:18 +0200

    feat: Support payload on tasks

--- src/metronome/models/job.go
+++ src/metronome/models/job.go
@@ -1,31 +1,41 @@
 package models
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"strconv"
 	"strings"
 
 	"github.com/Shopify/sarama"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/ovh/metronome/src/metronome/kafka"
 )
 
 // Job is a task execution.
 type Job struct {
-	GUID    string `json:"guid"`
-	UserID  string `json:"user_id"`
-	At      int64  `json:"at"`
-	Epsilon int64  `json:"epsilon"`
-	URN     string `json:"URN"`
+	GUID    string                 `json:"guid"`
+	UserID  string                 `json:"user_id"`
+	At      int64                  `json:"at"`
+	Epsilon int64                  `json:"epsilon"`
+	URN     string                 `json:"URN"`
+	Payload map[string]interface{} `json:"payload"`
 }
 
 // ToKafka serialize a Job to Kafka.
 func (j *Job) ToKafka() *sarama.ProducerMessage {
+	payloadBytes, err := json.Marshal(j.Payload)
+	if err != nil {
+		log.WithError(err).Warn("Cannot marshall job payload")
+		payloadBytes = []byte("{}")
+	}
+	p := base64.StdEncoding.EncodeToString(payloadBytes)
+
 	return &sarama.ProducerMessage{
 		Topic: kafka.TopicJobs(),
 		Key:   sarama.StringEncoder(j.GUID),
-		Value: sarama.StringEncoder(fmt.Sprintf("%v %v %v %v %v", j.GUID, j.UserID, j.At, j.Epsilon, j.URN)),
+		Value: sarama.StringEncoder(fmt.Sprintf("%v %v %v %v %v %v", j.GUID, j.UserID, j.At, j.Epsilon, j.URN, p)),
 	}
 }
 
@@ -33,7 +43,7 @@ func (j *Job) ToKafka() *sarama.ProducerMessage {
 func (j *Job) FromKafka(msg *sarama.ConsumerMessage) error {
 	key := string(msg.Key)
 	segs := strings.Split(string(msg.Value), " ")
-	if len(segs) != 5 {
+	if len(segs) != 6 {
 		return fmt.Errorf("unprocessable job(%v) - bad segments", key)
 	}
 
@@ -45,6 +55,16 @@ func (j *Job) FromKafka(msg *sarama.ConsumerMessage) error {
 	epsilon, err := strconv.ParseInt(segs[3], 0, 64)
 	if err != nil {
 		return fmt.Errorf("unprocessable job(%v) - bad epsilon", key)
+	}
+
+	var payload []byte
+	payload, err = base64.StdEncoding.DecodeString(segs[5])
+	if err != nil {
+		return fmt.Errorf("unprocessable job(%v) - bad payload (not base64)", key)
+	}
+	err = json.Unmarshal(payload, &j.Payload)
+	if err != nil {
+		return fmt.Errorf("unprocessable job(%v) - bad payload (not map string-interface)", key)
 	}
 
 	j.GUID = key
